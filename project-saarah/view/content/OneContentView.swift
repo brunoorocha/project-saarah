242d4865800b57e8f83bf18658facd33b5c9f19a
Guilherme Colombini <j.guilherme.c.silva@gmail.com>
2019-09-02 09:10:49 -0300

    build folder hierarchy

---
+++ project-saarah/view/content/OneContentView.swift
@@ -0,0 +1,9 @@
+//
+//  OneContentView.swift
+//  project-saarah
+//
+//  Created by Guilherme Colombini on 02/09/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import Foundation
