42dd4c24eeeb24840d9f32fd03a7b29e6273a55d
Guilherme Colombini <j.guilherme.c.silva@gmail.com>
2019-09-02 09:39:12 -0300

    productsview added into productsvc

---
+++ project-saarah/controller/ProductsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProductsViewController.swift
+//  project-saarah
+//
+//  Created by Guilherme Colombini on 02/09/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ProductsViewController: UIViewController, ConfigurableController {
+	var contentView: UIView? = ProductsView()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		setupContentView()
+	}
+}
---
+++ project-saarah/view/content/ProductsView.swift
@@ -0,0 +1,36 @@
+//
+//  ProductsView.swift
+//  project-saarah
+//
+//  Created by Guilherme Colombini on 02/09/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ProductsView: UIView {
+	
+	init() {
+		super.init(frame: .zero)
+		
+		instantiateViews()
+		buildViewHierarchy()
+		setupConstraints()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	func instantiateViews() {
+		
+	}
+	
+	func buildViewHierarchy() {
+		
+	}
+	
+	func setupConstraints() {
+		
+	}
+}
