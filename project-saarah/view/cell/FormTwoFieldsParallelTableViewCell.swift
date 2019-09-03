b1be93dfc50461ef295115c8799e19602526e124
Guilherme Colombini <j.guilherme.c.silva@gmail.com>
2019-09-03 09:32:05 -0300

    formtwofields layout done

---
+++ project-saarah/view/cell/FormTwoFieldsParallelTableViewCell.swift
@@ -0,0 +1,57 @@
+//
+//  FormTwoFieldsParallelTableViewCell.swift
+//  project-saarah
+//
+//  Created by Guilherme Colombini on 03/09/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import UIKit
+
+class FormTwoFieldsParallelTableViewCell: UITableViewCell {
+	var fieldNameLabel: UILabel!
+	var getDataTextField: UITextField!
+	
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		
+		selectionStyle = .none
+		
+		instantiateViews()
+		buildViewsHierarchy()
+		setupConstraints()
+	}
+	
+	func setContent() {
+		fieldNameLabel.text = "Campo do form"
+		getDataTextField.placeholder = "recebe campo do form"
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	func instantiateViews() {
+		fieldNameLabel = UILabel(frame: .zero)
+		fieldNameLabel.translatesAutoresizingMaskIntoConstraints = false
+		getDataTextField = UITextField(frame: .zero)
+		getDataTextField.translatesAutoresizingMaskIntoConstraints = false
+	}
+	
+	func buildViewsHierarchy() {
+		contentView.addSubview(fieldNameLabel)
+		contentView.addSubview(getDataTextField)
+	}
+	
+	func setupConstraints() {
+		NSLayoutConstraint.activate([
+			fieldNameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+			fieldNameLabel.centerYAnchor.constraint(equalTo: getDataTextField.centerYAnchor),
+			getDataTextField.widthAnchor.constraint(equalTo: contentView.widthAnchor),
+			getDataTextField.leadingAnchor.constraint(equalTo: fieldNameLabel.trailingAnchor),
+			getDataTextField.topAnchor.constraint(equalTo: contentView.topAnchor),
+			getDataTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+			getDataTextField.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+		])
+	}
+}
