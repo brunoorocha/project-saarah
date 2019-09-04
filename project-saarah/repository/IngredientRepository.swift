f497d5a69337e83413ea988c11cd8158050ec6e7
Thiago Valente <thiagoavalente@icloud.com>
2019-09-04 09:26:19 -0300

    Added CoreDao to crud data in coreData and added ingredient repository

---
+++ project-saarah/repository/IngredientRepository.swift
@@ -0,0 +1,9 @@
+//
+//  IngredientRepository.swift
+//  project-saarah
+//
+//  Created by Thiago Valente on 04/09/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import Foundation
