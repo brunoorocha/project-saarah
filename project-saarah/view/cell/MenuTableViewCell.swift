cc602379cd27f0717b139545d27b9fe6d5c11615
Marcus Vinícius <veras.marcusvinicius@gmail.com>
2019-09-09 09:44:07 -0300

    content-files-added-to-project

---
+++ project-saarah/view/cell/MenuTableViewCell.swift
@@ -0,0 +1,103 @@
+//
+//  MenuTableViewCell.swift
+//  project-saarah
+//
+//  Created by Marcus Vinícius on 09/09/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+    var dish: Dish? {
+        didSet {
+            guard let unwrappedDish = dish else { return }
+            
+            dishImageName.image = UIImage(named: unwrappedDish.dishImageName)
+            dishName.text = unwrappedDish.dishName
+            dishDescription.text = unwrappedDish.dishDescription
+            dishPrice.text = "\(unwrappedDish.dishPrice)"
+        }
+    }
+    
+    private let dishImageName: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 16.0
+        
+        // This enables autolayout to imageView.
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    private let dishName: UILabel = Label(frame: .zero)
+    private let dishDescription: UILabel = Label(frame: .zero)
+    private let dishPrice: UILabel = Label(frame: .zero)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = .white
+        
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        // This is a auxiliar view for the main content.
+        let contentView: UIView = {
+            let view = UIView()
+            view.backgroundColor = .blue
+            
+            view.layer.masksToBounds = true
+            view.layer.cornerRadius = 16.0
+            
+            // This enables autolayout to view.
+            view.translatesAutoresizingMaskIntoConstraints = false
+            
+            return view
+        }()
+        
+        // Add subviews to cell.
+        addSubviews([contentView, dishImageName])
+        
+        // Add subviews to containerView.
+        contentView.addSubviews([dishName, dishDescription, dishPrice])
+        
+        // Constraints for containerView, dishImageName, dishName, dishDescription, dishPrice.
+        NSLayoutConstraint.activate(
+            [
+                contentView.topAnchor.constraint(equalTo: topAnchor, constant: 8.0),
+                contentView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8.0),
+                contentView.leadingAnchor.constraint(equalTo: dishImageName.leadingAnchor, constant: 48.0),
+                contentView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16.0),
+                
+                dishImageName.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8.0),
+                dishImageName.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8.0),
+                dishImageName.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16.0),
+                dishImageName.widthAnchor.constraint(equalToConstant: 96.0),
+                
+                dishName.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8.0),
+                dishName.bottomAnchor.constraint(equalTo: contentView.centerYAnchor),
+                dishName.leadingAnchor.constraint(equalTo: dishImageName.trailingAnchor, constant: 8.0),
+                dishName.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8.0),
+                
+                dishDescription.topAnchor.constraint(equalTo: contentView.centerYAnchor, constant: 8.0),
+                dishDescription.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8.0),
+                dishDescription.leadingAnchor.constraint(equalTo: dishImageName.trailingAnchor, constant: 8.0),
+                dishDescription.trailingAnchor.constraint(equalTo: dishPrice.leadingAnchor, constant: -8.0),
+                
+                dishPrice.topAnchor.constraint(equalTo: contentView.centerYAnchor, constant: 8.0),
+                dishPrice.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8.0),
+                dishPrice.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8.0),
+                dishPrice.widthAnchor.constraint(equalToConstant: 64.0)
+            ]
+        )
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder aDecoder: ) has not been implemented.")
+    }
+}
---
+++ project-saarah/view/content/MenuTableViewHeader.swift
@@ -0,0 +1,91 @@
+//
+//  MenuTableViewHeader.swift
+//  project-saarah
+//
+//  Created by Marcus Vinícius on 09/09/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewHeader: UIView {
+    private let classifierImageName: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 8.0
+        
+        // This enables autolayout to imageView.
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    private let classifierName: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .red
+        
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 4.0
+        
+        // This enables autolayout to label.
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .green
+        
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        // This is a auxiliar view for the main content.
+        let contentView: UIView = {
+            let view = UIView()
+            view.backgroundColor = .blue
+            
+            view.layer.masksToBounds = true
+            view.layer.cornerRadius = 8.0
+            
+            // This enables autolayout to view.
+            view.translatesAutoresizingMaskIntoConstraints = false
+            
+            return view
+        }()
+        
+        // Add subviews to class.
+        addSubviews([contentView, classifierImageName])
+        
+        // Add subview to contentView.
+        contentView.addSubview(classifierName)
+        
+        // Contraints for contentView, classifierImageName, classifierName.
+        NSLayoutConstraint.activate(
+            [
+                contentView.topAnchor.constraint(equalTo: topAnchor, constant: 8.0),
+                contentView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8.0),
+                contentView.leadingAnchor.constraint(equalTo: classifierImageName.leadingAnchor, constant: 20.0),
+                contentView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16.0),
+                
+                classifierImageName.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4.0),
+                classifierImageName.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -4.0),
+                classifierImageName.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16.0),
+                classifierImageName.widthAnchor.constraint(equalToConstant: 40.0),
+                
+                classifierName.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8.0),
+                classifierName.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8.0),
+                classifierName.leadingAnchor.constraint(equalTo: classifierImageName.trailingAnchor, constant: 8.0),
+                classifierName.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8.0)
+            ]
+        )
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder aDecoder: ) has not been implemented.")
+    }
+}
