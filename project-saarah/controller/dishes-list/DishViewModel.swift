ff0358fcfd9c029d180e7986010b1071ddea49ec
Guilherme Colombini <j.guilherme.c.silva@gmail.com>
2019-09-10 09:33:06 -0300

    dishviewmodel basic done

---
+++ project-saarah/controller/dishes-list/DishViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  DishViewModel.swift
+//  project-saarah
+//
+//  Created by Guilherme Colombini on 10/09/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import UIKit
+
+struct DishViewModel {
+	var name: String
+	var image: UIImage
+	var description: String
+	var price: Double
+}
