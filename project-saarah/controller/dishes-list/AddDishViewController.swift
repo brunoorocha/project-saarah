b1b5410a7e6ffa0c8f2695b988b618e0f95ae930
Guilherme Colombini <j.guilherme.c.silva@gmail.com>
2019-09-10 09:48:12 -0300

    adddishview basic done

---
+++ project-saarah/controller/dishes-list/AddDishViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddDishViewController.swift
+//  project-saarah
+//
+//  Created by Guilherme Colombini on 10/09/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import UIKit
+
+class AddDishViewController: UIViewController, ConfigurableController {
+	var contentView: UIView? = AddDishView()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		setupContentView()
+	}
+}
