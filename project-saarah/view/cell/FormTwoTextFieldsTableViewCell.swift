7805dd75b800bdf94372492a2c9ac3a9d387a5a9
Guilherme Colombini <j.guilherme.c.silva@gmail.com>
2019-09-12 10:31:35 -0300

    implementando logica do campo do form com 2 test fields

--- project-saarah/view/cell/FormTwoTextFieldsTableViewCell.swift
+++ project-saarah/view/cell/FormTwoTextFieldsTableViewCell.swift
@@ -8,4 +8,61 @@
 
 import UIKit
 
-class 
+class FormTwoTextFieldsTableViewCell: UITableViewCell {
+	var fieldOneTextField: UITextField!
+	var fieldTwoTextField: UITextField!
+	var quantityTypeLabel: UILabel!
+	
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		
+		selectionStyle = .none
+		
+		instantiateViews()
+		buildViewsHierarchy()
+		setupConstraints()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	func instantiateViews() {
+		fieldOneTextField = UITextField()
+		fieldOneTextField.translatesAutoresizingMaskIntoConstraints = false
+		fieldOneTextField.delegate = self
+		fieldTwoTextField = UITextField()
+		fieldTwoTextField.translatesAutoresizingMaskIntoConstraints = false
+		fieldTwoTextField.delegate = self
+		
+	}
+	
+	func buildViewsHierarchy() {
+		contentView.addSubview(fieldOneTextField)
+		contentView.addSubview(fieldTwoTextField)
+	}
+	
+	func setupConstraints() {
+		NSLayoutConstraint.activate([
+			fieldOneTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+			fieldOneTextField.topAnchor.constraint(equalTo: contentView.topAnchor),
+			fieldOneTextField.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+			fieldTwoTextField.leadingAnchor.constraint(equalTo: fieldOneTextField.trailingAnchor),
+			fieldTwoTextField.topAnchor.constraint(equalTo: contentView.topAnchor),
+			fieldTwoTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+			fieldTwoTextField.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+		])
+	}
+}
+
+extension FormTwoTextFieldsTableViewCell: UITextFieldDelegate {
+	func textFieldDidEndEditing(_ textField: UITextField) {
+		if textField == inputDataTextField {
+			let text = textField.text
+			
+			if let key = formData?.key {
+				formData?.fieldValue[key] = text
+			}
+		}
+	}
+}
