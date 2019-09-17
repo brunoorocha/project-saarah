74b99997a89c17cb0154002d6b1e6f19dc354595
Guilherme Colombini <j.guilherme.c.silva@gmail.com>
2019-09-17 10:59:22 -0300

    menuitemproduct created

---
+++ project-saarah/models/MenuItemProduct.swift
@@ -0,0 +1,19 @@
+//
+//  MenuItemProduct.swift
+//  project-saarah
+//
+//  Created by Guilherme Colombini on 17/09/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import Foundation
+
+class MenuItemProduct {
+	let product: Product
+	let quantity: Double
+	
+	init(product: Product, quantity: Double) {
+		self.product = product
+		self.quantity = quantity
+	}
+}
