210bb9a321fb7efda317fecf5abf3d91d0e11bfb
Guilherme Colombini <j.guilherme.c.silva@gmail.com>
2019-09-17 11:03:16 -0300

    menuitem created

---
+++ project-saarah/models/MenuItem.swift
@@ -0,0 +1,27 @@
+//
+//  MenuItem.swift
+//  project-saarah
+//
+//  Created by Guilherme Colombini on 17/09/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import UIKit
+
+class MenuItem {
+	let id: Int
+	let name: String
+	let price: Double
+	let image: UIImage?
+	let onMenu: Bool
+	let products: [MenuItemProduct]
+	
+	init(id: Int, name: String, price: Double, image: UIImage, onMenu: Bool, products: [MenuItemProduct]) {
+		self.id = id
+		self.name = name
+		self.price = price
+		self.image = image
+		self.onMenu = onMenu
+		self.products = products
+	}
+}
