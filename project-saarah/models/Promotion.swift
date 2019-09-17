6e3db2b7f917338b2a23daebd02383f02ac84c7f
Guilherme Colombini <j.guilherme.c.silva@gmail.com>
2019-09-17 11:15:52 -0300

    promotion created

---
+++ project-saarah/models/Promotion.swift
@@ -0,0 +1,35 @@
+//
+//  Promotion.swift
+//  project-saarah
+//
+//  Created by Guilherme Colombini on 17/09/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import UIKit
+
+class Promotion {
+	let id: Int
+	let menuItemPromotions: [MenuItemPromotion]
+	let name: String
+	let price: Double
+	let image: UIImage?
+	let startDay: Date
+	let endDay: Date
+	let startHour: Date
+	let endHour: Date
+	let days: [Int]
+	
+	init(id: Int, menuItemPromotions: [MenuItemPromotion], name: String, price: Double, image: UIImage?, startDay: Date, endDay: Date, startHour: Date, endHour: Date, days: [Int]) {
+		self.id = id
+		self.menuItemPromotions = menuItemPromotions
+		self.name = name
+		self.price = price
+		self.image = image
+		self.startDay = startDay
+		self.endDay = endDay
+		self.startHour = startHour
+		self.endHour = endHour
+		self.days = days
+	}
+}
