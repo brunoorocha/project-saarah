cd6d7691e50797abc0b89b6b615e2bf89e3919c6
Marcus Vinícius <veras.marcusvinicius@gmail.com>
2019-09-25 09:12:31 -0300

    create-uiview-extension-file

---
+++ project-saarah/extensions/UIView.swift
@@ -0,0 +1,17 @@
+//
+//  UIView.swift
+//  project-saarah
+//
+//  Created by Marcus Vinícius on 25/09/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    public func addSubviews(_ views: [UIView]) {
+        views.forEach { (view) in
+            self.addSubview(view)
+        }
+    }
+}
