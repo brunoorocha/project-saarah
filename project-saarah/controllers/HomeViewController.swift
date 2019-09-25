684a3668ae631ec23221103e15a8bdda9809f624
Marcus Vinícius <veras.marcusvinicius@gmail.com>
2019-09-25 10:14:55 -0300

    main-component-build-by-the-uiview-extension-method

--- project-saarah/controllers/HomeViewController.swift
+++ project-saarah/controllers/HomeViewController.swift
@@ -9,55 +9,11 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    private let productItemCell: UIView = {
-        let view = UIView()
-        view.backgroundColor = .red
-        
-        // This enables autolayout to view.
-        view.translatesAutoresizingMaskIntoConstraints = false
-        
-        return view
-    }()
-    
-    private let amountLabel: UILabel = {
-        let label = UILabel()
-        label.backgroundColor = .green
-        
-        //This enables autolayout to label.
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        return label
-    }()
-    
-    private let validityLabel: UILabel = {
-        let label = UILabel()
-        label.backgroundColor = .green
-        
-        // This enables autolayout to label.
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        return label
-    }()
-    
-    private let valueLabel: UILabel = {
-        let label = UILabel()
-        label.backgroundColor = .green
-        
-        // This enables autolayout to label.
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        return label
-    }()
-    
-    private let addedDayLabel: UILabel = {
-        let label = UILabel()
-        label.backgroundColor = .green
-        
-        // This enables autolayout to label.
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        return label
-    }()
+    private let productItemCell = UIView(backgroundColor: .red)
+    private let amountLabel = UILabel(backgroundColor: .green)
+    private let validityLabel = UILabel(backgroundColor: .green)
+    private let valueLabel = UILabel(backgroundColor: .green)
+    private let addedDayLabel = UILabel(backgroundColor: .green)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,41 +28,56 @@ class HomeViewController: UIViewController {
         view.addSubview(productItemCell)
         
         // Add subviews to productItemCell.
-        productItemCell.addSubviews([addedDayLabel, valueLabel, validityLabel, amountLabel])
+        productItemCell.addSubviews([amountLabel, validityLabel, valueLabel, addedDayLabel])
         
         // Constraints for productItemCell.
-        NSLayoutConstraint.activate(
-            [
-                productItemCell.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16.0),
-                productItemCell.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16.0),
-                productItemCell.widthAnchor.constraint(equalToConstant: 343.0),
-                productItemCell.heightAnchor.constraint(equalToConstant: 176)
-            ]
+        productItemCell.anchor(
+            top: view.safeAreaLayoutGuide.topAnchor,
+            leading: view.safeAreaLayoutGuide.leadingAnchor,
+            bottom: nil,
+            trailing: nil,
+            padding: UIEdgeInsets(top: 16.0, left: 16.0, bottom: 0.0, right: 0.0),
+            size: CGSize(width: 343.0, height: 176.0)
         )
         
         // Contraints for amountLabel.
-        NSLayoutConstraint.activate(
-            [
-                addedDayLabel.topAnchor.constraint(equalTo: valueLabel.bottomAnchor, constant: 16.0),
-                addedDayLabel.leadingAnchor.constraint(equalTo: productItemCell.leadingAnchor, constant: 16.0),
-                addedDayLabel.widthAnchor.constraint(equalToConstant: 120.0),
-                addedDayLabel.heightAnchor.constraint(equalToConstant: 24.0),
-                
-                valueLabel.topAnchor.constraint(equalTo: validityLabel.bottomAnchor, constant: 16.0),
-                valueLabel.leadingAnchor.constraint(equalTo: productItemCell.leadingAnchor, constant: 16.0),
-                valueLabel.widthAnchor.constraint(equalToConstant: 120.0),
-                valueLabel.heightAnchor.constraint(equalToConstant: 24.0),
-                
-                validityLabel.topAnchor.constraint(equalTo: amountLabel.bottomAnchor, constant: 16.0),
-                validityLabel.leadingAnchor.constraint(equalTo: productItemCell.leadingAnchor, constant: 16.0),
-                validityLabel.widthAnchor.constraint(equalToConstant: 120.0),
-                validityLabel.heightAnchor.constraint(equalToConstant: 24.0),
-                
-                amountLabel.topAnchor.constraint(equalTo: productItemCell.topAnchor, constant: 16.0),
-                amountLabel.leadingAnchor.constraint(equalTo: productItemCell.leadingAnchor, constant: 16.0),
-                amountLabel.widthAnchor.constraint(equalToConstant: 120.0),
-                amountLabel.heightAnchor.constraint(equalToConstant: 24.0)
-            ]
+        amountLabel.anchor(
+            top: productItemCell.topAnchor,
+            leading: productItemCell.leadingAnchor,
+            bottom: nil,
+            trailing: nil,
+            padding: UIEdgeInsets(top: 16.0, left: 16.0, bottom: 0.0, right: 0.0),
+            size: CGSize(width: 120.0, height: 24.0)
+        )
+        
+        // Contraints for validityLabel.
+        validityLabel.anchor(
+            top: amountLabel.bottomAnchor,
+            leading: productItemCell.leadingAnchor,
+            bottom: nil,
+            trailing: nil,
+            padding: UIEdgeInsets(top: 16.0, left: 16.0, bottom: 0.0, right: 0.0),
+            size: CGSize(width: 120.0, height: 24.0)
+        )
+        
+        // Contraints for valueLabel.
+        valueLabel.anchor(
+            top: validityLabel.bottomAnchor,
+            leading: productItemCell.leadingAnchor,
+            bottom: nil,
+            trailing: nil,
+            padding: UIEdgeInsets(top: 16.0, left: 16.0, bottom: 0.0, right: 0.0),
+            size: CGSize(width: 120.0, height: 24.0)
+        )
+        
+        // Contraints for addedDayLabel.
+        addedDayLabel.anchor(
+            top: valueLabel.bottomAnchor,
+            leading: productItemCell.leadingAnchor,
+            bottom: nil,
+            trailing: nil,
+            padding: UIEdgeInsets(top: 16.0, left: 16.0, bottom: 0.0, right: 0.0),
+            size: CGSize(width: 120.0, height: 24.0)
         )
     }
 }
