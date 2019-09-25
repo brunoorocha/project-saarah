ef24046c4d12260ccb577ac7fa298ddff61b3dd0
Marcus Vinícius <veras.marcusvinicius@gmail.com>
2019-09-25 09:46:43 -0300

    main-labels-added-to-component-view

--- project-saarah/controllers/HomeViewController.swift
+++ project-saarah/controllers/HomeViewController.swift
@@ -19,6 +19,46 @@ class HomeViewController: UIViewController {
         return view
     }()
     
+    private let amountLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .green
+        
+        //This enables autolayout to label.
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let validityLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .green
+        
+        // This enables autolayout to label.
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let valueLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .green
+        
+        // This enables autolayout to label.
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let addedDayLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .green
+        
+        // This enables autolayout to label.
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,8 +68,11 @@ class HomeViewController: UIViewController {
     }
     
     private func setupLayout() {
-        // Add subviews to view.
-        view.addSubviews([productItemCell])
+        // Add subview to view.
+        view.addSubview(productItemCell)
+        
+        // Add subviews to productItemCell.
+        productItemCell.addSubviews([addedDayLabel, valueLabel, validityLabel, amountLabel])
         
         // Constraints for productItemCell.
         NSLayoutConstraint.activate(
@@ -37,7 +80,32 @@ class HomeViewController: UIViewController {
                 productItemCell.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16.0),
                 productItemCell.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16.0),
                 productItemCell.widthAnchor.constraint(equalToConstant: 343.0),
-                productItemCell.heightAnchor.constraint(equalToConstant: 170)
+                productItemCell.heightAnchor.constraint(equalToConstant: 176)
+            ]
+        )
+        
+        // Contraints for amountLabel.
+        NSLayoutConstraint.activate(
+            [
+                addedDayLabel.topAnchor.constraint(equalTo: valueLabel.bottomAnchor, constant: 16.0),
+                addedDayLabel.leadingAnchor.constraint(equalTo: productItemCell.leadingAnchor, constant: 16.0),
+                addedDayLabel.widthAnchor.constraint(equalToConstant: 120.0),
+                addedDayLabel.heightAnchor.constraint(equalToConstant: 24.0),
+                
+                valueLabel.topAnchor.constraint(equalTo: validityLabel.bottomAnchor, constant: 16.0),
+                valueLabel.leadingAnchor.constraint(equalTo: productItemCell.leadingAnchor, constant: 16.0),
+                valueLabel.widthAnchor.constraint(equalToConstant: 120.0),
+                valueLabel.heightAnchor.constraint(equalToConstant: 24.0),
+                
+                validityLabel.topAnchor.constraint(equalTo: amountLabel.bottomAnchor, constant: 16.0),
+                validityLabel.leadingAnchor.constraint(equalTo: productItemCell.leadingAnchor, constant: 16.0),
+                validityLabel.widthAnchor.constraint(equalToConstant: 120.0),
+                validityLabel.heightAnchor.constraint(equalToConstant: 24.0),
+                
+                amountLabel.topAnchor.constraint(equalTo: productItemCell.topAnchor, constant: 16.0),
+                amountLabel.leadingAnchor.constraint(equalTo: productItemCell.leadingAnchor, constant: 16.0),
+                amountLabel.widthAnchor.constraint(equalToConstant: 120.0),
+                amountLabel.heightAnchor.constraint(equalToConstant: 24.0)
             ]
         )
     }
