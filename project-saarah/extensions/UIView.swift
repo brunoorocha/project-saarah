e18a754bbd9650d7e4b05a09dc859a91b7c30515
Marcus Vinícius <veras.marcusvinicius@gmail.com>
2019-09-25 09:59:45 -0300

    uiview-extension-file-modified

--- project-saarah/extensions/UIView.swift
+++ project-saarah/extensions/UIView.swift
@@ -9,9 +9,73 @@
 import UIKit
 
 extension UIView {
+    convenience public init(backgroundColor: UIColor = .clear) {
+        self.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+    }
+    
+    public struct AnchoredConstraints {
+        var top: NSLayoutConstraint?
+        var leading: NSLayoutConstraint?
+        var bottom: NSLayoutConstraint?
+        var trailing: NSLayoutConstraint?
+        var width: NSLayoutConstraint?
+        var height: NSLayoutConstraint?
+    }
+    
     public func addSubviews(_ views: [UIView]) {
         views.forEach { (view) in
             self.addSubview(view)
         }
+    }
+    
+    @discardableResult
+    public func anchor(
+        top: NSLayoutYAxisAnchor?,
+        leading: NSLayoutXAxisAnchor?,
+        bottom: NSLayoutYAxisAnchor?,
+        trailing: NSLayoutXAxisAnchor?,
+        padding: UIEdgeInsets = .zero,
+        size: CGSize = .zero
+    ) -> AnchoredConstraints {
+        // This enables autolayout to class UIView.
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        var anchoredConstraints = AnchoredConstraints()
+        
+        if let topAnchor = top {
+            anchoredConstraints.top = self.topAnchor.constraint(equalTo: topAnchor, constant: padding.top)
+        }
+        
+        if let leadingAnchor = leading {
+            anchoredConstraints.leading = self.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding.left)
+        }
+        
+        if let bottomAnchor = bottom {
+            anchoredConstraints.bottom = self.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -padding.bottom)
+        }
+        
+        if let trailingAnchor = trailing {
+            anchoredConstraints.trailing = self.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding.right)
+        }
+        
+        if size.width != 0 {
+            anchoredConstraints.width = self.widthAnchor.constraint(equalToConstant: size.width)
+        }
+        
+        if size.height != 0 {
+            anchoredConstraints.height = self.heightAnchor.constraint(equalToConstant: size.height)
+        }
+        
+        [
+            anchoredConstraints.top,
+            anchoredConstraints.leading,
+            anchoredConstraints.bottom,
+            anchoredConstraints.trailing,
+            anchoredConstraints.width,
+            anchoredConstraints.height
+        ].forEach { $0?.isActive = true }
+        
+        return anchoredConstraints
     }
 }
