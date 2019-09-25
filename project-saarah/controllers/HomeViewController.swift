6bd6ed66f482affc3cbbcb2d6209a56b2352cbbb
Marcus Vinícius <veras.marcusvinicius@gmail.com>
2019-09-25 09:26:10 -0300

    view-added-to-home-view-controller

--- project-saarah/controllers/HomeViewController.swift
+++ project-saarah/controllers/HomeViewController.swift
@@ -9,7 +9,36 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    private let productItemCell: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        
+        // This enables autolayout to view.
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        // Add subviews to view.
+        view.addSubviews([productItemCell])
+        
+        // Constraints for productItemCell.
+        NSLayoutConstraint.activate(
+            [
+                productItemCell.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16.0),
+                productItemCell.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16.0),
+                productItemCell.widthAnchor.constraint(equalToConstant: 343.0),
+                productItemCell.heightAnchor.constraint(equalToConstant: 170)
+            ]
+        )
     }
 }
