98ebec07a28749b7eb11d9c3f12df9e258fe7cd7
Thiago Valente <thiagoavalente@icloud.com>
2019-10-16 18:46:16 -0300

    removed useless code from router

--- project-saarah/scenes/select-product-measurement/SelectProductMeasurementRouter.swift
+++ project-saarah/scenes/select-product-measurement/SelectProductMeasurementRouter.swift
@@ -19,26 +19,4 @@ protocol SelectProductMeasurementDataPassing {
 class SelectProductMeasurementRouter: NSObject, SelectProductMeasurementRoutingLogic, SelectProductMeasurementDataPassing {
 	weak var viewController: SelectProductMeasurementViewController?
 	var dataStore: SelectProductMeasurementDataStore?
-
-	// MARK: Routing
-//	func routeToSomewhere() {
-//		let destinationVC = SomewhereViewController()
-//		guard var destinationDataStore = destinationVC.router?.dataStore else { return }
-//
-//		guard let dataStore = dataStore else { return }
-//		guard let viewController = viewController else { return }
-//
-//		passDataToSomewhere(source: dataStore, destination: &destinationDataStore)
-//		navigateToSomewhere(source: viewController, destination: destinationVC)
-//	}
-//
-//	// MARK: Passing data
-//	func passDataToSomewhere(source: SelectProductMeasurementDataStore, destination: inout SomewhereDataStore) {
-//		destination.name = source.name
-//	}
-//
-//	// MARK: Navigation
-//	func navigateToSomewhere(source: SelectProductMeasurementViewController, destination: SomewhereViewController) {
-//		source.show(destination, sender: nil)
-//	}
 }
