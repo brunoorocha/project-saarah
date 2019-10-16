b0153ff2a37e8e16d91af0c2866f099749f6f036
Thiago Valente <thiagoavalente@icloud.com>
2019-10-16 18:45:15 -0300

    Added view to scene select produt measure

--- project-saarah/scenes/select-product-measurement/SelectProductMeasurementView.swift
+++ project-saarah/scenes/select-product-measurement/SelectProductMeasurementView.swift
@@ -9,26 +9,48 @@
 import UIKit
 
 class SelectProductMeasurementView: UIView {
-	init() {
-		super.init(frame: .zero)
 
-		backgroundColor = UIColor.white
+    let tableView = UITableView()
 
-		instantiateViews()
-		buildViewsHierarchy()
-		setupConstraints()
-	}
+    init() {
+        super.init(frame: .zero)
 
-	required init?(coder aDecoder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
+        backgroundColor = UIColor.white
+        tableView.backgroundColor = AppStyleGuide.Colors.background.uiColor
 
-	func instantiateViews() {
-	}
+        instantiateViews()
+        buildViewsHierarchy()
+        setupConstraints()
+    }
 
-	func buildViewsHierarchy() {
-	}
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
-	func setupConstraints() {
-	}
+    func instantiateViews() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        tableView.register(SelectableCell.self, forCellReuseIdentifier: "selectableTableViewCell")
+    }
+
+    func buildViewsHierarchy() {
+        addSubview(tableView)
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+
+    func buildCell(indexPath: IndexPath, name: String) -> UITableViewCell {
+        let cell = SelectableCell()
+        cell.roundCellIfNeeded(index: indexPath.row, numberOfCells: tableView.numberOfRows(inSection: indexPath.section))
+        cell.label.text = name
+
+        return cell
+    }
 }
