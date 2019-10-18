a23cd68c54606e2dd1e009f8c07ccf74745f4383
Thiago Valente <thiagoavalente@icloud.com>
2019-10-18 02:47:44 -0300

    Added product worker and mock to product item

---
+++ project-saarah/utils/extensions/Date+Formatter.swift
@@ -0,0 +1,15 @@
+//
+//  Date+DDMMYYYY.swift
+//  project-saarah
+//
+//  Created by Thiago Valente on 18/10/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    func formDate() -> String {
+        
+    }
+}
---
+++ project-saarah/utils/extensions/Double+RoundToDecimal.swift
@@ -0,0 +1,9 @@
+//
+//  Double+RoundToDecimal.swift
+//  project-saarah
+//
+//  Created by Thiago Valente on 18/10/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import Foundation
