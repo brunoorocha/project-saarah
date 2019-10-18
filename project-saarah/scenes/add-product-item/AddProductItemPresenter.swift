2a2c16fdfdefe87dfef1f2c2bab218843d70d121
Guilherme Colombini <j.guilherme.c.silva@gmail.com>
2019-10-17 21:05:21 -0300

    presenter configurated

--- project-saarah/scenes/add-product-item/AddProductItemPresenter.swift
+++ project-saarah/scenes/add-product-item/AddProductItemPresenter.swift
@@ -9,15 +9,20 @@
 import Foundation
 
 protocol AddProductItemPresentationLogic {
-	func presentSomething(response: AddProductItem.Something.Response)
+	func presentAPIResponse(response: AddProductItem.AddItem.Response)
 }
 
 class AddProductItemPresenter: AddProductItemPresentationLogic {
 	weak var viewController: AddProductItemDisplayLogic?
 
 	// MARK: Do something
-	func presentSomething(response: AddProductItem.Something.Response) {
-//		let viewModel = AddProductItem.Something.ViewModel()
-//		viewController?.displaySomething(viewModel: viewModel)
+	func presentAPIResponse(response: AddProductItem.AddItem.Response) {
+		switch (response.apiMessage) {
+		case "success":
+			let viewModel = AddProductItem.AddItem.ViewModel.AddItemViewModel(success: true, title: "Sucesso", message: "O novo item do seu produto foi salvo na nuvem.")
+			viewController?.displayAPIResponse(viewModel: viewModel)
+		default:
+			break
+		}
 	}
 }
