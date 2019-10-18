d9bd839329b079c85b0bd03afe5d8c6749773c68
Thiago Valente <thiagoavalente@icloud.com>
2019-10-18 00:26:08 -0300

    Added ProductItemTableViewDataSource

---
+++ project-saarah/scenes/product-item/ProductItemTableViewDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  ProductItemTableViewDataSource.swift
+//  project-saarah
+//
+//  Created by Thiago Valente on 17/10/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import Foundation
