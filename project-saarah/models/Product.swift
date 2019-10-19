d136a11f252a2c2d45fefa6a91e2550fc4681e30
Bruno Rocha <mail.brunorocha@gmail.com>
2019-10-19 09:36:30 -0300

    Extending Product and Measure classes from Decodable and created its coding keys

--- project-saarah/models/Product.swift
+++ project-saarah/models/Product.swift
@@ -8,23 +8,42 @@
 
 import Foundation
 
-class Product {
-	let id: Int
+class Product: Decodable {
+	let id: String
 	let name: String
 	let measure: Measure
 	let barCode: String?
 	let logs: [ProductLog]
 
-	init(id: Int, name: String, measure: Measure, barCode: String?, logs: [ProductLog]) {
+	init(id: String, name: String, measure: Measure, barCode: String?, logs: [ProductLog]) {
 		self.id = id
 		self.name = name
 		self.measure = measure
 		self.barCode = barCode
 		self.logs = logs
 	}
+    
+    required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(String.self, forKey: .id)
+        name = try values.decode(String.self, forKey: .name)
+        barCode = try values.decodeIfPresent(String.self, forKey: .barCode)
+        measure = try values.decode(Measure.self, forKey: .measurement)
+        logs = []
+    }
 
     // TODO: - insert a real count quantity function
     func quantity() -> String {
         return "10"
+    }
+}
+
+extension Product {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case measurement
+        case barCode
+        case logs
     }
 }
