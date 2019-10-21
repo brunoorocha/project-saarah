355167ddf1913e71994215b32b5f8990aaa57420
Thiago Valente <thiagoavalente@icloud.com>
2019-10-21 11:11:24 -0300

    Removed old button table view cell and added the new with project pattern

---
+++ project-saarah/views/components/custom-cells/ButtonTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ButtonTableViewCell.swift
+//  project-saarah
+//
+//  Created by Thiago Valente on 21/10/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import Foundation
