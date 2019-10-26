8bf81de2ff1598879694790ceacf10ce0bbe5bb9
Marcus Vinícius <veras.marcusvinicius@gmail.com>
2019-10-26 16:13:42 -0300

    another-travis-problem-probably-fixed

--- project-saarah/utils/UIColor+rgbaExtension.swift
+++ project-saarah/utils/UIColor+rgbaExtension.swift
@@ -31,15 +31,15 @@ extension UIColor {
 
     static func setupColorFor(_ lightMode: UIColor, _ darkMode: UIColor) -> UIColor {
         if #available(iOS 13, *) {
-            return UIColor.init { (trait) -> UIColor in
+            return UIColor.init(dynamicProvider: { (trait) -> UIColor in
                 let isDarkMode = trait.userInterfaceStyle
-
+                
                 if isDarkMode == .dark {
                     return darkMode
                 } else {
                     return lightMode
                 }
-            }
+            })
         } else {
             return lightMode
         }
