df905bd0a5622e9b729ec94ec1b5fcbeaa3157c0
Guilherme Colombini <j.guilherme.c.silva@gmail.com>
2019-11-05 11:09:13 -0300

    request for sign up done

---
+++ project-saarah/services/account-service/ApiAccountStore.swift
@@ -0,0 +1,18 @@
+//
+//  ApiAccountStore.swift
+//  project-saarah
+//
+//  Created by Guilherme Colombini on 05/11/19.
+//  Copyright © 2019 Bruno Rocha. All rights reserved.
+//
+
+import Foundation
+
+class ApiAccountStore: AccountStore {
+	func signUp(name: String, email: String, password: String, confirmPassword: String, _ completion: @escaping (Result<CreateAccount.SignUpResponse?, NetworkServiceError>) -> Void) {
+		let networkService = NetworkService()
+		networkService.request(endpoint: ConeheadApiEndpoint.signUp(name: name, email: email, password: password, confirmPassword: confirmPassword)) { (result: Result<CreateAccount.SignUpResponse?, NetworkServiceError>) in
+			completion(result)
+		}
+	}
+}
