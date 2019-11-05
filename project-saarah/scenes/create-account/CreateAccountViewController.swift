fc5b6515470c552e63eb478b1b396dd466c0f74b
Guilherme Colombini <j.guilherme.c.silva@gmail.com>
2019-11-05 08:37:03 -0300

    showing sign up form

--- project-saarah/scenes/create-account/CreateAccountViewController.swift
+++ project-saarah/scenes/create-account/CreateAccountViewController.swift
@@ -19,6 +19,7 @@ class CreateAccountViewController: UIViewController, CreateAccountDisplayLogic {
 
 	// MARK: Controller Property
 	private var contentView = CreateAccountView()
+	private let tableViewDataSource = CreateAccountTableViewDataSource()
 
 	// MARK: View lifecycle
 	override func viewDidLoad() {
@@ -39,7 +40,18 @@ class CreateAccountViewController: UIViewController, CreateAccountDisplayLogic {
 
 	func setupContentView() {
 		title = "CreateAccount"
-		view = contentView
+		contentView.tableView.delegate = self
+		contentView.tableView.dataSource = self
+		tableViewDataSource.registerCells(for: contentView.tableView)
+		contentView.tableView.reloadData()
+		
+		view.addSubview(contentView)
+		NSLayoutConstraint.activate([
+			contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			contentView.topAnchor.constraint(equalTo: view.topAnchor),
+			contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+		])
 	}
 
 	// MARK: Do something
@@ -49,5 +61,26 @@ class CreateAccountViewController: UIViewController, CreateAccountDisplayLogic {
 	}
 
 	func displaySomething(viewModel: CreateAccount.Something.ViewModel) {
+	}
+}
+
+extension CreateAccountViewController: UITableViewDelegate, UITableViewDataSource {
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return tableViewDataSource.numberOfSections()
+	}
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return tableViewDataSource.numberOfRows(in: section)
+	}
+
+	func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+		return tableViewDataSource.viewForHeader(in: section)
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let reuseIdentifier = tableViewDataSource.reuseIdentifier(for: indexPath.section)
+		let reusableCell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+		let cell = tableViewDataSource.modify(reusableCell, for: indexPath)
+		return cell
 	}
 }
